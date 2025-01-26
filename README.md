8938f13b3a251095c0c0ddfc2866362f4c7cf104
d <paviad@sunypoly.edu>
2025-01-26 17:24:28 -0500

    Setup for class, cannot find header files VKSetup.hpp and VKRender.hpp

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CS 450 (Computer Graphics)
 ***Spring 2025***  
-***Author: <Your Name Here>***  
+***Author: Derrick Pavia***  
 ***Original Author: Dr. Michael J. Reale***  
 ***SUNY Polytechnic Institute*** 
 
--- src/app/Assign01.cpp
+++ src/app/Assign01.cpp
@@ -6,8 +6,8 @@ int main(int argc, char **argv) {
     cout << "BEGIN FORGING!!!" << endl;
 
     // Set name
-    string appName = "BasicVulkan";
-    string windowTitle = "Basic Vulkan Example";
+    string appName = "Assign01";
+    string windowTitle = "Assign01: paviad";
     int windowWidth = 800;
     int windowHeight = 600;
 
