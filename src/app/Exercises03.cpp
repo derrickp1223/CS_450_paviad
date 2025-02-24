f81a2d36ed1909afe2f4b2db3160ef2db0aeed99
d <paviad@sunypoly.edu>
2025-02-24 13:59:16 -0500

    Class exercise

--- src/app/Exercises03.cpp
+++ src/app/Exercises03.cpp
@@ -5,7 +5,21 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include "VKUtility.hpp"
+#include "VKBuffer.hpp"
 using namespace std;
+
+struct PointVertex {
+    glm::vec3 pos;
+};
+
+vector<PointVertex> vertices = {
+    {{-0.5f, -0.5f, 0.5f}},
+    {{ 0.5f, -0.5f, 0.5f}},
+    {{ 0.5f,  0.5f, 0.5f}},
+    {{-0.5f,  0.5f, 0.5f}}
+};
+
+vector<unsigned int> indices = {0, 2, 1 ,2, 0, 3};
 
 int main(int argc, char **argv) {
 
@@ -120,6 +134,113 @@ int main(int argc, char **argv) {
     auto fragSrc = readBinaryFile(
         "build/compiledshaders/ProfExercises03/shader.frag.spv");
         
+    vk::ShaderModule vertShader = createVulkanShaderModule(device, vertSrc);
+    vk::ShaderModule fragShader = createVulkanShaderModule(device, fragSrc);
+    
+    vk::PipelineShaderStageCreateInfo shaderStages [] = {
+        vk::PipelineShaderStageCreateInfo(
+            {}, vk::ShaderStageFlagBits::eVertex, vertShader, "main"
+        ),
+        vk::PipelineShaderStageCreateInfo(
+            {}, vk::ShaderStageFlagBits::eFragment, fragShader, "main"
+        )
+    };
+
+    vk::DeviceSize vertBufferSize = sizeof(vertices[0])*vertices.size();
+    vk::DeviceSize indBufferSize = sizeof(indices[0])*indices.size();
+
+    VulkanBuffer vkVertices = createVulkanBuffer(
+        phyDevice, device, vertBufferSize, 
+        vk::BufferUsageFlagBits::eVertexBuffer,
+        vk::MemoryPropertyFlagBits::eHostVisible
+        | vk::MemoryPropertyFlagBits::eHostCoherent
+    );
+    VulkanBuffer vkIndices = createVulkanBuffer(
+        phyDevice, device, indBufferSize, 
+        vk::BufferUsageFlagBits::eIndexBuffer,
+        vk::MemoryPropertyFlagBits::eHostVisible
+        | vk::MemoryPropertyFlagBits::eHostCoherent
+    );
+
+    copyDataToVulkanBuffer(device, 
+                           vkVertices.memory,
+                           vertBufferSize, 
+                           vertices.data());   
+    copyDataToVulkanBuffer(device, 
+                           vkIndices.memory,
+                           indBufferSize, 
+                           indices.data());
+
+    vk::VertexInputBindingDescription vertBindDesc 
+        = vk::VertexInputBindingDescription(
+            0, 
+            sizeof(vertices[0], vk::VertexInputRate::eVertex)
+        );
+    
+    vector<vk::VertexInputAttributeDescription> vertAttribDesc = {
+        vk::VertexInputAttributeDescription(
+            0, 0,
+            vk::Format::eR32G32B32Sfloat,
+            offsetof(PointVertex, pos))
+    };
+
+    vk::PipelineVertexInputStateCreateInfo vertexInputInfo(
+        {}, vertBindDesc, vertAttribDesc
+    );
+
+    vk::PipelineInputAssemblyStateCreateInfo inputAssembly(
+        {}, vk::PrimitiveTopology::eTriangleList, false
+    );
+
+
+    vk::Viewport viewport(
+        0, 0, (float)swapextent.width, (float)swapextent.height,
+        0.0f, 1.0f
+    );
+    vk::Rect2D scissors({0,0}, swapextent);
+    vk::PipelineViewportStateCreateInfo viewportInfo(
+        {}, viewport, scissors
+    );
+
+    vector<vk::DynamicState> dynamicStates = {
+        vk::DynamicState::eViewport,
+        vk::DynamicState::eScissor
+    };
+    vk::PipelineDynamicStateCreateInfo dynamicInfo(
+        {}, dynamicStates
+    );
+
+    vk::PipelineRasterizationStateCreateInfo rasterizer {};
+    rasterizer.lineWidth = 1.0f;
+    rasterizer.cullMode = vk::CullModeFlagBits::eNone;
+    rasterizer.frontFace = vk::FrontFace::eCounterClockwise;
+
+    vk::PipelineColorBlendAttachmentState colorBlend {};
+    colorBlend.colorWriteMask = 
+        vk::ColorComponentFlagBits::eR |
+        vk::ColorComponentFlagBits::eG |
+        vk::ColorComponentFlagBits::eB |
+        vk::ColorComponentFlagBits::eA;
+    vk::PipelineColorBlendStateCreateInfo colorBlending = {
+        {}, false, vk::LogicOp::eCopy, colorBlend
+    };
+
+    vk::PipelineDepthStencilStateCreateInfo depthStencil = {
+        {}, true , true, vk::CompareOp::eLess,
+        false, false, {}, {}
+    };
+
+    vk::PipelineLayoutCreateInfo layoutinfo(
+        {}, {}, {}
+    );
+    vk::PipelineLayout pipelineLayout = device.createPipelineLayout(layoutInfo);
+
+
+    //PIPELINE TODO
+
+    device.destroyShaderModule(vertShader);
+    device.destroyShaderModule(fragShader);
+
     // CREATION TODO
 
     while(!glfwWindowShouldClose(window)) {
@@ -129,6 +250,9 @@ int main(int argc, char **argv) {
     }
 
     // CLEANUP TODO
+    cleanupVulkanBuffer(device,vkIndices);
+    cleanupVulkanBuffer(device,vkVertices);
+
     device.destroyRenderPass(pass);
     for(int i = 0; i < swapviews.size(); i++) {
         device.destroyImageView(swapviews.at(i));
